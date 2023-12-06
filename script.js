a089e9abfa39fcdabd404160f4d623957a64c713
Dilmuhammad12 <yyh11719@gmail.com>
2023-12-06 20:28:09 +0500

    full scale(1.1)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function asarlar_btn() {
-  alert('asarlar')
+  // alert('asarlar')
   const asarlar_div = document.querySelector('.asarlari .bottom');
   asarlar_div.classList.toggle('asarlar-notscroll')
 }
